dada186c4542dc26fff803be5a5739b8e80e5b2e
Edd Steel <edd@eddsteel.com>
2021-01-07 13:24:54 -0800

    Rebase on latest, add indicator for IME, remove symbols

--- keyboards/kbdfans/kbd75/keymaps/edd/keymap.c
+++ keyboards/kbdfans/kbd75/keymaps/edd/keymap.c
@@ -1,19 +1,14 @@
 #include QMK_KEYBOARD_H
-#include "process_unicode_common.h"
-
 extern rgblight_config_t rgblight_config;
 
 #define LT1S LT(1,KC_SPC)
 #define KILL LGUI(LALT(KC_ESC))
-#define UMLX UNICODE_MODE_LNX
-#define UMOX UNICODE_MODE_OSX
 #define NKRO MAGIC_TOGGLE_NKRO
 
 // layernames
 enum {
       _BASE = 0,
       _FUNC,
-      _SYMB,
       _MACR,
       NLYRS
 };
@@ -31,37 +26,38 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       } else {}
       break;
      case KC_IM:
-       tap_code16(HYPR(KC_SPC));
-       rgblight_set_layer_state(3, rgblight_get_layer_state(0));
+       if (record->event.pressed) {
+         tap_code16(HYPR(KC_SPC));
+         rgblight_set_layer_state(3, !rgblight_get_layer_state(3));
+       }
   }
   return true;
-};
-
-// light 10-13 red when input method is changed
-const rgblight_segment_t PROGMEM inputmethod_layer[] = RGBLIGHT_LAYER_SEGMENTS({10,4,HSV_RED});
-
-const rgblight_segment_t* const PROGMEM my_rgb_layers[] =
-  RGBLIGHT_LAYERS_LIST(RGBLIGHT_LAYER_SEGMENTS({0,15,12,255,255}),
-                       RGBLIGHT_LAYER_SEGMENTS({0,15,132,255,255}),
-                       RGBLIGHT_LAYER_SEGMENTS({0,15,220,255,255}),
-                       inputmethod_layer
-);
-
-
-void keyboard_post_init_user(void) {
-  rgblight_enable_noeeprom();
-  rgblight_sethsv_noeeprom(12, 225, 255);
-  rgblight_layers = my_rgb_layers;
 }
 
+// light 10-13 red when input method is changed
+const rgblight_segment_t PROGMEM inputmethod_layer[] = RGBLIGHT_LAYER_SEGMENTS({10,4,HSV_WHITE});
+
+const rgblight_segment_t PROGMEM rgblayer0[] = RGBLIGHT_LAYER_SEGMENTS({0,16,12,225,255});
+const rgblight_segment_t PROGMEM rgblayer1[] = RGBLIGHT_LAYER_SEGMENTS({0,16,132,225,255});
+const rgblight_segment_t PROGMEM rgblayer2[] = RGBLIGHT_LAYER_SEGMENTS({0,16,220,225,255});
+
+const rgblight_segment_t* const PROGMEM my_rgb_layers[] =
+  RGBLIGHT_LAYERS_LIST(rgblayer0, rgblayer1, rgblayer2, inputmethod_layer);
+
+
 uint32_t layer_state_set_user(uint32_t state) {
-  rgblight_set_layer_state(0, layer_state_cmp(state, 0));
+//  rgblight_set_layer_state(0, layer_state_cmp(state, 0));
   rgblight_set_layer_state(1, layer_state_cmp(state, 1));
   rgblight_set_layer_state(2, layer_state_cmp(state, 2));
  
   return state;
 }
 
+void keyboard_post_init_user(void) {
+  rgblight_enable_noeeprom();
+  rgblight_sethsv_noeeprom(12, 225, 255);
+  rgblight_layers = my_rgb_layers;
+}
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   LAYOUT(
@@ -77,7 +73,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_SLEP,       HYPR(KC_E),    HYPR(KC_F), HYPR(KC_M),  HYPR(KC_X), KC_F17,     KC_F18,     KC_F19,     KC_F20,     KC_F21,     KC_F22,      KC_F23,      KC_F24,     KC_INS,       KC_SLCK,         KILL,
     OSL(2),        RGB_TOG,       RGB_M_P,    RGB_M_B,     RGB_M_SW,   RGB_M_K,    RGB_M_G,    RGB_HUI,    RGB_HUD,    RGB_SAI,    RGB_SAD,     RGB_VAD,     RGB_VAI,    RESET,        RESET,           DEBUG,
     KC_NO,         HYPR(KC_Q),    HYPR(KC_W), HYPR(KC_E),  HYPR(KC_R), HYPR(KC_T), HYPR(KC_Y), HYPR(KC_U), HYPR(KC_I), HYPR(KC_O), HYPR(KC_P),  RGB_SPD,     RGB_SPI,    EEP_RST,                       KC_BRIU,
-    UMLX,          HYPR(KC_A),    HYPR(KC_S), HYPR(KC_D),  HYPR(KC_F), HYPR(KC_G), HYPR(KC_H), HYPR(KC_J), HYPR(KC_K), HYPR(KC_L), KC_NO,       KC_NO,                              UMOX,            KC_BRID,
+    KC_NO,          HYPR(KC_A),    HYPR(KC_S), HYPR(KC_D),  HYPR(KC_F), HYPR(KC_G), HYPR(KC_H), HYPR(KC_J), HYPR(KC_K), HYPR(KC_L), KC_NO,       KC_NO,                              KC_NO,            KC_BRID,
+    KC_MUTE,       KC_NO,         HYPR(KC_Z), HYPR(KC_X),  HYPR(KC_C), HYPR(KC_V), HYPR(KC_B), HYPR(KC_N), HYPR(KC_M), KC_NO,      KC_NO,       KC_NO,       KC_MPLY,                  KC_VOLU,         NKRO,
+    OSL(2),        KC_NO,         KC_NO,                               KC_SLEP,    KC_IM,       KC_SLEP,                            KC_NO,       KC_NO,       KC_TRNS,    KC_MRWD,      KC_VOLD,         KC_MFFD
+         ),
+  LAYOUT(
+    KC_SLEP,       HYPR(KC_E),    HYPR(KC_F), HYPR(KC_M),  HYPR(KC_X), KC_F17,     KC_F18,     KC_F19,     KC_F20,     KC_F21,     KC_F22,      KC_F23,      KC_F24,     KC_INS,       KC_SLCK,         KILL,
+    OSL(2),        RGB_TOG,       RGB_M_P,    RGB_M_B,     RGB_M_SW,   RGB_M_K,    RGB_M_G,    RGB_HUI,    RGB_HUD,    RGB_SAI,    RGB_SAD,     RGB_VAD,     RGB_VAI,    RESET,        RESET,           DEBUG,
+    KC_NO,         HYPR(KC_Q),    HYPR(KC_W), HYPR(KC_E),  KC_MR, HYPR(KC_T), HYPR(KC_Y), HYPR(KC_U), HYPR(KC_I), HYPR(KC_O), HYPR(KC_P),  RGB_SPD,     RGB_SPI,    EEP_RST,                       KC_BRIU,
+    KC_NO,          HYPR(KC_A),    HYPR(KC_S), HYPR(KC_D),  HYPR(KC_F), HYPR(KC_G), HYPR(KC_H), HYPR(KC_J), HYPR(KC_K), HYPR(KC_L), KC_NO,       KC_NO,                              KC_NO,            KC_BRID,
     KC_MUTE,       KC_NO,         HYPR(KC_Z), HYPR(KC_X),  HYPR(KC_C), HYPR(KC_V), HYPR(KC_B), HYPR(KC_N), HYPR(KC_M), KC_NO,      KC_NO,       KC_NO,       KC_MPLY,                  KC_VOLU,         NKRO,
     OSL(2),        KC_NO,         KC_NO,                               KC_SLEP,    KC_IM,       KC_SLEP,                            KC_NO,       KC_NO,       KC_TRNS,    KC_MRWD,      KC_VOLD,         KC_MFFD
   )
